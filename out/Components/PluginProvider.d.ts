dc7ff0500d587c0c4ba86f09cbdaf0a5afc6fe59
AsynchronousAI <72946059+AsynchronousAI@users.noreply.github.com>
2025-07-19 14:46:41 -0500

    Bindings for PluginProvider.d.ts

---
+++ out/Components/PluginProvider.d.ts
@@ -0,0 +1,41 @@
+/// <reference types="@rbxts/types" />
+
+import { ReactNode } from "@rbxts/react";
+
+/**
+ * Interface for the PluginProvider component's props.
+ * 
+ * @within PluginProvider
+ * @tag Component Props
+ */
+export interface PluginProviderProps {
+	/** The root plugin instance (typically the global `plugin` variable). */
+	Plugin: Plugin;
+
+	/** The child components rendered within the context of the provider. */
+	children: ReactNode;
+}
+
+/**
+ * Provides an interface to plugin APIs for other components in the tree.
+ *
+ * Only needed if you're using components that require access to the plugin context,
+ * like the `Splitter` or using the `useMouseIcon` hook.
+ *
+ * Typically rendered at the root of your plugin UI component tree.
+ *
+ * @example
+ * ```tsx
+ * const MyComponent = () => (
+ *   <StudioComponents.PluginProvider Plugin={plugin}>
+ *     <MyExample />
+ *   </StudioComponents.PluginProvider>
+ * );
+ * ```
+ *
+ * @param props The props for PluginProvider
+ * @returns A React element
+ */
+declare function PluginProvider(props: PluginProviderProps): React.Element;
+
+export = PluginProvider;
